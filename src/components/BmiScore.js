317e19cd731a9f4e2f5e80c9c4276f86b2a9386c
sinsha <sinshasb@gmail.com>
2024-04-02 15:50:26 +0530

    bmi calculation and type completed

--- src/components/BmiScore.js
+++ src/components/BmiScore.js
@@ -1,11 +1,22 @@
 
-const BmiScore = (props) => {
-  console.log(props,'propsss')
+const BmiScore = ({bmiNo,bmiName}) => {
+  
   return (
-    <div>
-      bmi score {props.bmiNo}<br/>
-      bmi type {props.bmiName}
+    <>
+     <div className="text-center shadow rounded p-4">
+      <div>Your BMI Score</div>
+      <div className="row justify-content-md-center">
+        <div className="p-3 my-2 alert fs-1 alert-primary col-sm-4">{bmiNo}</div>
+      </div>
+      <div className="fs-3 fw-bold text-primary">{bmiName}</div>
+      
+        {/* <div className="fs-4">"You need lose <span className="fw-bold"> kg"</span> </div>
+     
+        <div className="fs-4">"You need gain <span className="fw-bold"> kg"</span> </div>
+        <div className="fs-4">"You weight is Normal,Good for you" </div> */}
     </div>
+    
+    </>
   )
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,69 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Form = () => {
-  
+const Form = ({getData}) => {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [alert, setAlert] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (isNaN(weight) || isNaN(height)) {
+      console.log("Not a valid input");
+      setAlert(true)
+    } else {
+      getData(weight,height)
+      setAlert(false)
+      console.log(weight, height);
+    }
+  };
   return (
     <>
-    form
-      
-    </>
-  )
-}
+      <div className="col-sm-4 shadow rounded px-5">
+        <h1 className="text-center pt-3 text-secondary h2">BMI Calculator</h1>
+        <form onSubmit={onSubmit}>
+          <div className="row">
+            <div className="col col-sm-6">
+              <div className="my-3">
+                <label className="form-label">Weight(kg) :</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={weight}
+                  onChange={(e) => {
+                    setWeight(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="col col-sm-6">
+              <div className="my-3">
+                <label className="form-label">Hight(m) :</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  required
+                  value={height}
+                  onChange={(e) => {
+                    setHeight(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary my-3"
+            value="Get BMI"
+          />
+        </form>
+        {alert && <div className="alert alert-danger" role="alert">
+          Plz enter valid datas
+        </div>}
 
-export default Form
+        
+      </div>
+    </>
+  );
+};
+
+export default Form;
